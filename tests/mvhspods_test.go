a5a46e82684ac5c5927d54faa6974e9810928d8a
milind-u <milind.upadhyay@gmail.com>
2021-11-11 16:26:33 -0800

    Improve tests

--- tests/mvhspods_test.go
+++ tests/mvhspods_test.go
@@ -1,33 +1,40 @@
 package tests
 
 import (
-	"math"
-	"testing"
+  "math"
+  "testing"
 
-	"mvhspods"
+  "mvhspods"
 )
 
-func TestPods(t *testing.T) {
-	// TODO: for some reason one pod exceeds this and has ~0.37, and way too many blach kids
-	const percentDiffTolerance mvhspods.Percent = 0.3
+func TestDiversity(t *testing.T) {
+  const percentErrTolerance mvhspods.Percent = 0.37
+  const avgPercentErrTolerance mvhspods.Percent = 0.12
 
-	students := GenerateStudents(100)
-	pm := mvhspods.PodManager{
-		Headers:  Headers,
-		Students: students,
-	}
-	pm.MakePods(mvhspods.DefaultPodSize, false)
-	for i, pod := range pm.Pods() {
-		if i != mvhspods.EldPod {
-			percents := mvhspods.PercentsOf(pod)
-			for field, percent := range pm.Population() {
-				if diff := math.Abs(float64(percents[field] - percent)); diff > float64(percentDiffTolerance) {
-					t.Error("Percent difference of", diff,
-						"between population percent", percent, "and pod percent", percents[field],
-						",\nfor field", field, ", exceeds tolerance of", percentDiffTolerance,
-						"\nPod number", i+1, "-", pod)
-				}
-			}
-		}
-	}
+  students := GenerateStudents(500)
+  pm := mvhspods.PodManager{Headers: Headers, Students: students}
+  pm.MakePods(mvhspods.DefaultPodSize, false)
+
+  var avgErr mvhspods.Percent
+  for i, pod := range pm.Pods() {
+    if i != mvhspods.EldPod {
+      percents := mvhspods.PercentsOf(pod)
+      for field, percent := range pm.Population() {
+        err := mvhspods.Percent(math.Abs(float64(percents[field] - percent)))
+        avgErr += err
+        if err > percentErrTolerance {
+          t.Error("Percent error of", err,
+            "between population percent", percent, "and pod percent", percents[field],
+            ",\nfor field", field, ", exceeds tolerance of", percentErrTolerance,
+            "\nPod number", i+1, "-", pod)
+        }
+      }
+    }
+  }
+
+  avgErr /= mvhspods.Percent(len(pm.Population()) * len(pm.Pods()))
+  t.Log("Average error:", avgErr)
+  if avgErr > avgPercentErrTolerance {
+    t.Error("Average error exceeds tolerance of", avgPercentErrTolerance)
+  }
 }
