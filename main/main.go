0de6dba6a2c655ee427a433c15623549461fe918
milind-u <milind.upadhyay@gmail.com>
2021-12-04 22:10:20 -0800

    Add web glue

--- main/main.go
+++ main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "flag"
+  "syscall/js"
 
   "mvhspods"
   "tests"
@@ -9,7 +10,27 @@ import (
   "github.com/milind-u/glog"
 )
 
+func webMain() js.Func {
+  return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+    pods := ""
+    if len(args) == 1 {
+      csv := args[0].String()
+
+      var pm mvhspods.PodManager
+      pm.ReadStudentsFromString(csv)
+      pm.MakePods(mvhspods.DefaultPodSize)
+      glog.Infoln("ELD stats:", tests.PodStats(&pm.Eld))
+      glog.Infoln("Stats:", tests.PodStats(&pm.PodData))
+      pods = pm.WritePodsToString()
+    } else {
+      glog.Errorln("Expected 1 arg (csv), but got", len(args))
+    }
+    return pods
+  })
+}
+
 func main() {
+  web := flag.Bool("web", true, "Whether to run the program for the webapp (if false, CLI)")
   studentsToGenerate := flag.Int("students_to_generate", 0,
     "If set, will generate the given number of random students instead of making pods")
   podSize := flag.Int("pod_size", mvhspods.DefaultPodSize, "Number of students per pod")
@@ -21,7 +42,11 @@ func main() {
 
   glog.SetSeverity(glog.InfoSeverity)
 
-  if *studentsToGenerate != 0 {
+  if *web {
+    js.Global().Set("makePods", webMain())
+    // Keep the program running
+    <-make(chan interface{})
+  } else if *studentsToGenerate != 0 {
     flag.Parse()
     glog.SetSeverity(glog.InfoSeverity)
 
